e6975ee424f927af0fdff11d11308bf87f637445
Christopho <christopho@solarus-games.org>
2018-11-27 21:03:35 +0100

    Rebame color conversion scripts

--- tools/ntsc-to-pal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 ]; then
-  echo 'No files specified'
-  exit 1
-fi
-
-gimp -i -b "$(cat ntsc-to-pal.scm)" -b "(ntsc-to-pal \"$@\")" -b "(gimp-quit 0)"
-
--- tools/pal-to-ntsc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 ]; then
-  echo 'No files specified'
-  exit 1
-fi
-
-gimp -i -b "$(cat pal-to-ntsc.scm)" -b "(pal-to-ntsc \"$@\")" -b "(gimp-quit 0)"
-
