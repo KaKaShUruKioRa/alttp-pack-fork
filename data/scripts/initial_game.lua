c3880b5e967d0b8f6694b43e60b8a89fff223f13
Medhoc <mehdi.abelha@gmail.com>
2019-01-10 18:54:47 +0100

    Menu : Implement equipment & quest items in the menu, add quest items
    Implement equipement & quest items in the menus, add quests items
    
    Fix starting point

--- data/scripts/initial_game.lua
+++ data/scripts/initial_game.lua
@@ -22,6 +22,7 @@ function initial_game:initialize_new_savegame(game)
   game:set_max_money(100)
   game:set_ability("lift", 0)
   game:set_ability("sword", 1)
+  game:get_item("equipment/tunic"):set_variant(1)
   
   -- default command binding
   game:set_value("keyboard_save", "escape")
